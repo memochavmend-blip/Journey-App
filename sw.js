791574f7abcf516303c16283f7a2a6fbe7e03a78
memochavmend-blip <memochavmend@gmail.com>
2026-09-25 18:55:42 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    Al desplegar una versión nueva: subir VERSION (cambia los bytes del archivo) para que el navegador
    detecte el SW nuevo, limpie la caché vieja y (con la recarga del cliente) entregue el HTML nuevo. */
 
-const VERSION = "practica-v7-2";
+const VERSION = "denha-v7-3";
 const SHELL = ["./", "./index.html"];
 
 self.addEventListener("install", e => {
